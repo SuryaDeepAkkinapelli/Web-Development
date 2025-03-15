29e91ee4f9ca8087bb0342ef634cdc29cea6ab61
SuryaDeepAkkinapelli <suryaakkinapelli@gmail.com>
2025-03-15 16:29:34 +0530

    Javascript

---
+++ 01Introduction/script.js
@@ -0,0 +1,21 @@
+alert("Hello World");
+console.log("Hello world!");
+console.log("Hello world!!");
+console.log("Hello world!!!");
+
+var a=prompt("Enter your number:");
+console.log("Your number is "+a);
+
+var isTrue=confirm("Are you sure to leave this page and blast your computer")
+if(isTrue){
+    console.log("Blasting your computer!!")
+}
+else{
+    console.log("Not blasting your computer!!")
+
+}
+
+document.title="Javascript";
+
+document.body.style.backgroundColor = "red";
+//This will add inline CSS
